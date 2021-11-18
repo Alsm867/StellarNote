5df1cb7bd6ba5ee201e97371db773206a24e0ef4
sam <ortegachan867@gmail.com>
2021-11-17 19:13:21 -0500

    did it on the main branch again but the good thing is nothing is broken

--- frontend/src/components/NotesPageComponent/index.js
+++ frontend/src/components/NotesPageComponent/index.js
@@ -3,12 +3,18 @@ import { useDispatch, useSelector } from "react-redux";
 import "./NotesPage.css";
 
 
-function TheNotes(){
 
+function TheNotes(){
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const notes = useSelector(state => state.notes);
+    const notebooks = useSelector(state => state.notebooks);
 
     return(
         <div className="main-notes">
-            <div className='notebook-bar'></div>
+            <div className='notebook-bar'>
+
+            </div>
         </div>
     )
 }
