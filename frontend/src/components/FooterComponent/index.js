b5c31dcc84b4faec012efcba9062211f1358f7bd
sam <ortegachan867@gmail.com>
2022-02-08 22:14:03 -0500

    changed cursor

--- frontend/src/components/FooterComponent/index.js
+++ frontend/src/components/FooterComponent/index.js
@@ -11,7 +11,7 @@ function FooterComponent() {
             <div className='sam'>
             Sam:
             </div>
-                <a href='https://github.com/Alsm867'><img className='g-hub' src='https://res.cloudinary.com/dzjkwepju/image/upload/v1637101398/Styckr/Untitled_design_o5b62v.png' alt='ghub' width='30px' height='30px'></img></a>
+                <div ><a className='g-point' href='https://github.com/Alsm867'><img className='g-hub' src='https://res.cloudinary.com/dzjkwepju/image/upload/v1637101398/Styckr/Untitled_design_o5b62v.png' alt='ghub' width='30px' height='30px'></img></a></div>
                 <a href='https://www.linkedin.com/in/sam-ortega-66677921a/'><img className='linked' src='https://res.cloudinary.com/dzjkwepju/image/upload/v1637101453/Styckr/Untitled_design_1_eohmqp.png' alt='linked' width='30px' height='30px'></img></a>
         <About/>
         </div>
